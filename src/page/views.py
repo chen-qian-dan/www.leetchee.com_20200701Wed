aba8c42c5fb2ef174af295ff9cd970635a992b01
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-01 18:56:28 +0930

    step 13_page_create app templates

--- src/page/views.py
+++ src/page/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
 def page_home(request):
-    return HttpResponse('<h1>Page Home</h1>')
+    #return HttpResponse('<h1>Page Home</h1>')
+    return render(request, 'page/page_home.html')
 
 
 def page_about(request):
-    return HttpResponse('<h1>Page About</h1>')
+    #return HttpResponse('<h1>Page About</h1>')
+    return render(request, 'page/page_about.html')
