6d233f905a2cd66e76588a392915820b5dbe3dd1
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-04 17:02:19 +0930

    apply base on blog home page

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Qian',
+        'title': 'ML',
+        'content': 'Machine Learning', 
+        'date_posted': 'July 02, 2020',
+    }, 
+    {
+        'author': 'Chen',
+        'title': 'CV',
+        'content': 'Computer Vision', 
+        'date_posted': 'July 02, 2020',
+    }
+]
+
+
 def blog_home(request):
     #return HttpResponse('<h1>Page Home</h1>')
-    return render(request, 'blog/blog_home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/blog_home.html', context)
