ac0bdc23d818633e78c6ff1817f98609deeb1f12
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-09 16:53:54 +0930

    register_flush message after user creation

--- src/user/views.py
+++ src/user/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib import messages 
+
 
 
 # Create your views here.
 
 def user_register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'已生成账号：{username}!')
+            return redirect('page_home')
+    else:
+        form = UserCreationForm()
     return render(request, "user/user_register.html", {'form': form})
     
