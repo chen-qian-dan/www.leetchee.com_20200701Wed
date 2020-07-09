7dbdddcb5a02c0495fa4652243bcae512ce3a390
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-09 23:11:43 +0930

    user_database_seperate image folder for each app

--- src/leetchee/settings.py
+++ src/leetchee/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # tell crispy forms which css forms we want to use 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- src/user/models.py
+++ src/user/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='user_profile_pic_default.jpg', upload_to='user/profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='user/profile_pics')
 
     # tell what to print out
     def __str__(self):
