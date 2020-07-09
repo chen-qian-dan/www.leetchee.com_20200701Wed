a566ce0c9ba901f73cf1dbd5682c20694400b056
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-09 23:49:59 +0930

    user_profile_edit profile.html with image

--- src/leetchee/urls.py
+++ src/leetchee/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static 
 from user import views as user_views
 
 urlpatterns = [
@@ -30,3 +32,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/user_login.html'), name='user_login'), 
     path('logout/', auth_views.LogoutView.as_view(template_name='user/user_logout.html'), name='user_logout'), 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
