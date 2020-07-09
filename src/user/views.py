174a8f3d78b3c4fce0e41eaa5d6931a98ec21eda
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-09 20:47:45 +0930

    register_add new field to register form

--- src/user/views.py
+++ src/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm 
 from django.contrib import messages 
+from .forms import UserRegisterForm 
 
 
 
@@ -8,13 +8,13 @@ from django.contrib import messages
 
 def user_register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'已生成账号：{username}!')
             return redirect('page_home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, "user/user_register.html", {'form': form})
     
