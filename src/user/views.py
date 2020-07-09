832fc8e8fbd95acbebfcbffe3bbc318742a12d38
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-09 21:50:54 +0930

    register_direct to login page after register instead of home page

--- src/user/views.py
+++ src/user/views.py
@@ -12,9 +12,10 @@ def user_register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'已生成账号：{username}!')
-            return redirect('page_home')
+            messages.success(request, f'已成功为您注册新账号： {username} ! 您现可用此账号登陆。')
+            return redirect('user_login')
     else:
         form = UserRegisterForm()
-    return render(request, "user/user_register.html", {'form': form})
+    context = {'form': form}
+    return render(request, "user/user_register.html", context)
     
